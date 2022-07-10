9fc1cb216c66ee2573d11ede1ef9967562ab040f
theobarretosilva <barretotheo25@gmail.com>
2022-07-10 16:25:22 -0300

    Adicionando exercicios

---
+++ Exercícios/Semana2/Exercicios Semanais/Contador/index.js
@@ -0,0 +1,9 @@
+function decrementar(){
+    var numAtual = document.getElementById("num").textContent;
+    document.getElementById("num").textContent = --numAtual;
+}
+
+function incrementar(){
+    var numAtual = document.getElementById("num").textContent;
+    document.getElementById("num").textContent = ++numAtual;
+}
