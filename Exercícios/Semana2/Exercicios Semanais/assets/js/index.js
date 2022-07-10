d9e5fd6d9485e3bd5a4154a7e7016851a2f588b3
theobarretosilva <barretotheo25@gmail.com>
2022-07-10 15:00:55 -0300

    Adicionando exercícios

---
+++ Exercícios/Semana2/Exercicios Semanais/assets/js/index.js
@@ -0,0 +1,13 @@
+function mostrar(){
+    var name = document.querySelector("#nome");
+    var nome = name.value;
+
+    var Email = document.querySelector("#email");
+    var email = Email.value;
+
+    var Descricao = document.querySelector("#descricao");
+    var descricao = Descricao.value;
+    
+    console.log(
+    "Formulário de contato => Nome: " + nome + " - E-mail: " + email + " - Descrição: " + descricao);
+}
