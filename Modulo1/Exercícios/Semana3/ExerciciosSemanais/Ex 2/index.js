2eaebddf9c9d0ec84f790cb9ef69e70c9dfc8fcb
theobarretosilva <barretotheo25@gmail.com>
2022-07-13 15:22:48 -0300

    Atualizando os exercicios

---
+++ Modulo1/Exercícios/Semana3/ExerciciosSemanais/Ex 2/index.js
@@ -0,0 +1,22 @@
+
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+];
+
+var list = [];
+
+listaConvidados.forEach( item =>{
+    var nomes = item.nome + " " + item.sobrenome
+    list.push(nomes);
+})
+
+console.log(list);
