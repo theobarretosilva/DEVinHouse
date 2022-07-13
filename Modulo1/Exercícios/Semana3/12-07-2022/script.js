295882a83f4f6d907d176535394996173cd086fd
theobarretosilva <barretotheo25@gmail.com>
2022-07-13 14:05:53 -0300

    Upload de exercicios

---
+++ Modulo1/Exercícios/Semana3/12-07-2022/script.js
@@ -0,0 +1,11 @@
+var carro = {
+    cor: "Prata",
+    modelo: "Hatch",
+    marca: "BMW",
+    placa: "A1B2C3",
+    ano: 2022,
+    tetoSolar: true,
+    motor: "v8",
+};
+
+console.table(carro);
---
+++ Modulo1/Exercícios/Semana3/ExerciciosSemanais/Ex 1/index.js
@@ -0,0 +1,8 @@
+
+var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ];
+
+function multiplicaPor2(item){
+    console.log(item*2)
+}
+
+listaNumeros.forEach(multiplicaPor2);
