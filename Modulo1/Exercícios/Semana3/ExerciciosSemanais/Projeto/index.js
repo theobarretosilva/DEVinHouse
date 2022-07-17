2c28be669af517d34d4bf91a23908bf46414cb3b
theobarretosilva <barretotheo25@gmail.com>
2022-07-17 20:05:54 -0300

    Atualizando os exercicios

--- Modulo1/Exercícios/Semana3/ExerciciosSemanais/Projeto/index.js
+++ Modulo1/Exercícios/Semana3/ExerciciosSemanais/Projeto/index.js
@@ -1,26 +1,25 @@
-var convidados = [
-    {nome: "Clarissa", sobrenome: "Gonçalves", setor: "Camarote", idade: 18},
-    {nome: "Eduarda", sobrenome: "Franderlinde", setor: "Camarote", idade: 17},
-    {nome: "Mariah", sobrenome: "Matos", setor: "Camarote", idade: 17},
-    {nome: "Raquel", sobrenome: "Barreto", setor: "Pista", idade: 32},
-    {nome: "Iran", sobrenome: "Silva", setor: "Pista", idade: 35},
-    {nome: "Guilherme", sobrenome: "Silva", setor: "Pista", idade: 15},
-    {nome: "Giovanna", sobrenome: "Grigio", setor: "Arquibancada", idade: 18},
-    {nome: "Marcelo", sobrenome: "Gomes", setor: "Arquibancada", idade: 20},
-    {nome: "Ricardo", sobrenome: "Spíndola", setor: "Arquibancada", idade: 17},
-]
-var listaDeConvidados = [convidados];
+var convidado1 = {nome: "Clarissa", sobrenome: "Gonçalves", setor: "Camarote", idade: 18};
+var convidado2 = {nome: "Eduarda", sobrenome: "Franderlinde", setor: "Camarote", idade: 17};
+
+var convidado3 = {nome: "Raquel", sobrenome: "Barreto", setor: "Pista", idade: 32};
+var convidado4 = {nome: "Iran", sobrenome: "Silva", setor: "Pista", idade: 35};
+
+var convidado5 = {nome: "Giovanna", sobrenome: "Grigio", setor: "Arquibancada", idade: 18};
+var convidado6 = {nome: "Ricardo", sobrenome: "Spíndola", setor: "Arquibancada", idade: 17};
+
+var listaDeConvidados = [convidado1, convidado2, convidado3, convidado4, convidado5, convidado6];
 
 
-function liberarBebidas(listaDeConvidados){*/
-    var listaDeConvidadosFinal = [];
-    var idadeObj = listaDeConvidados[convidados.idade];
-    
-    if(idadeObj > 17){
-        listaDeConvidados.openBar = true;
-    }else{
-        listaDeConvidados.openBar = false;
-    }
-    listaDeConvidadosFinal.push(idadeObj)
-    console.log(listaDeConvidados)
+function liberarBebidas(lista){
+    var resultado = lista.map((convidado) => {
+        convidado.openBar = false;
+        
+        if(listaDeConvidados.idade = 18){
+            convidado.openBar = false;
+        }
+        return convidado;
+    });
+    return resultado;
 }
+var listaDeConvidadosFinal = liberarBebidas(listaDeConvidados);
+console.log(listaDeConvidadosFinal);
