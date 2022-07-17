0c1144ff6933ea7223b6445e507d7ac8ba643fb6
theobarretosilva <barretotheo25@gmail.com>
2022-07-17 17:34:46 -0300

    Atualizando exercicios

--- Modulo1/Exercícios/Semana3/ExerciciosSemanais/Projeto/index.js
+++ Modulo1/Exercícios/Semana3/ExerciciosSemanais/Projeto/index.js
@@ -1,33 +1,26 @@
-var camarote = [
+var convidados = [
     {nome: "Clarissa", sobrenome: "Gonçalves", setor: "Camarote", idade: 18},
     {nome: "Eduarda", sobrenome: "Franderlinde", setor: "Camarote", idade: 17},
     {nome: "Mariah", sobrenome: "Matos", setor: "Camarote", idade: 17},
-];
-
-var pista = [
     {nome: "Raquel", sobrenome: "Barreto", setor: "Pista", idade: 32},
     {nome: "Iran", sobrenome: "Silva", setor: "Pista", idade: 35},
-    {nome: "Guilherme", sobrenome: "Silva", setor: "Pista", idade: 15}
-]
-
-var arquibancada = [
+    {nome: "Guilherme", sobrenome: "Silva", setor: "Pista", idade: 15},
     {nome: "Giovanna", sobrenome: "Grigio", setor: "Arquibancada", idade: 18},
     {nome: "Marcelo", sobrenome: "Gomes", setor: "Arquibancada", idade: 20},
     {nome: "Ricardo", sobrenome: "Spíndola", setor: "Arquibancada", idade: 17},
 ]
+var listaDeConvidados = [convidados];
 
-var listaDeConvidados = [camarote, pista, arquibancada];
-console.log(listaDeConvidados);
 
-function liberarBebidas(listaDeConvidados){
+function liberarBebidas(listaDeConvidados){*/
     var listaDeConvidadosFinal = [];
-    const idadeObj = listaDeConvidados[idade];
+    var idadeObj = listaDeConvidados[convidados.idade];
     
     if(idadeObj > 17){
         listaDeConvidados.openBar = true;
     }else{
         listaDeConvidados.openBar = false;
     }
-
-    console.log(listaDeConvidados);
+    listaDeConvidadosFinal.push(idadeObj)
+    console.log(listaDeConvidados)
 }
