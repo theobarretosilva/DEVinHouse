75a3fd06f262b2df4702b48bb5878eb52716c3c3
theobarretosilva <barretotheo25@gmail.com>
2022-08-10 18:14:09 -0300

    Exercicio 3 finalizado

--- Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/components/Rodape/Rodape.jsx
+++ Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/components/Rodape/Rodape.jsx
@@ -1,15 +1,26 @@
 export const Rodape = () => {
     return(
-        <div>
-            <p><strong>Nosso endereço:</strong> Rua Maurício Ribeiro dos paraguais, 840 - Cobertura</p>
+        <footer>
             <div>
-                <p><strong>Nossas redes sociais:</strong></p>
-                <ul>
-                    <li>
-                        <a></a>
-                    </li>
-                </ul>
+                <div>
+                    <h3><strong>CONTATO</strong></h3>
+                    <p>(48) 3265-4872</p>
+                    <p>restheorante@gmail.com</p>
+                </div>
+                <div>
+                    <h3><strong>HORÁRIO DE FUNCIONAMENTO</strong></h3>
+                    <p>Segunda à Sábado: 19:00 - 00:00</p>
+                    <p>Abrimos aos Feriados!</p>
+                    <p>Domingo: Fechado</p>
+                </div>
+                <div>
+                    <h3><strong>REDES SOCIAIS</strong></h3>
+                    <img src="src\assets\imgs\logoInstagram.png" alt="Logo do Instagram" />
+                    <img src="src\assets\imgs\logoWhatsapp.png" alt="Logo do WhatsApp" />
+                </div>
+                <p>Copyright &copy; 2022 Restheorante</p>
             </div>
-        </div>
+        </footer>
+        
     )
 }
