f343723cb537569b0fac1023e1ae9f00ed9e7c6d
theobarretosilva <barretotheo25@gmail.com>
2022-08-09 22:55:59 -0300

    Atualizando projeto

--- Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/App.jsx
+++ Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar';
+import { Rodape } from './components/Rodape/Rodape';
 import { Titulo } from './components/Titulo/Titulo';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Titulo />
+      <Rodape />
     </div>
   )
 }
---
+++ Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/components/Rodape/Rodape.jsx
@@ -0,0 +1,15 @@
+export const Rodape = () => {
+    return(
+        <div>
+            <p><strong>Nosso endereço:</strong> Rua Maurício Ribeiro dos paraguais, 840 - Cobertura</p>
+            <div>
+                <p><strong>Nossas redes sociais:</strong></p>
+                <ul>
+                    <li>
+                        <a></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
