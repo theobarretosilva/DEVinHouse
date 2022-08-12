c9ab33e2714e134260809a538412b9ebc5a3336c
theobarretosilva <barretotheo25@gmail.com>
2022-08-12 15:56:07 -0300

    Atualizando projeto

--- Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/App.jsx
+++ Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/App.jsx
@@ -1,23 +1,12 @@
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar';
 import { Rodape } from './components/Rodape/Rodape';
-import { Card } from './components/Card/Card';
 import { Secoes } from './components/Secoes/Secoes';
 
 function App() {
   return (
     <div className='divPrincipal'>
       <Navbar />
-      {/* <Card 
-        card = {{
-          img: 'https://www.dicasdemulher.com.br/wp-content/uploads/2021/05/entradas-para-jantar-00.png',
-          descricaoImg: "Bruschetta de tomates confit",
-          nomePrato: "Bruschetta de tomates confit",
-          descricaoPrato: "Bruschetta de tomates confit com manjericões frescos",
-          tempoPreparo: "Tempo de preparo: 15 minutos",
-          preco: "R$ 24,90",
-        }}
-      /> */}
       <Secoes />
       <Rodape />
     </div>
--- Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/components/Secoes/Secoes.jsx
+++ Modulo1/Exercícios/Semana7/exerciciosSemanais/projetoRestaurante/src/components/Secoes/Secoes.jsx
@@ -9,20 +9,26 @@ export const Secoes = () => {
             <div className='secoes'>
                 <div>
                     <h2 className='nomeSecao'>Entradas</h2>
+                        { <Card 
+                            card = {{
+                            img: 'https://www.dicasdemulher.com.br/wp-content/uploads/2021/05/entradas-para-jantar-00.png',
+                            descricaoImg: "Bruschetta de tomates confit",
+                            nomePrato: "Bruschetta de tomates confit",
+                            descricaoPrato: "Bruschetta de tomates confit com manjericões frescos",
+                            tempoPreparo: "Tempo de preparo: 15 minutos",
+                            preco: "R$ 24,90",
+                            }}
+                        /> }
                 </div>
-                <div className='linha'></div>
                 <div>
                     <h2 className='nomeSecao'>Saladas</h2>
                 </div>
-                <div className='linha'></div>
                 <div>
                     <h2 className='nomeSecao'>Pratos principais</h2>
                 </div>
-                <div className='linha'></div>
                 <div>
                     <h2 className='nomeSecao'>Sobremesas</h2>
                 </div>
-                <div className='linha'></div>
                 <div>
                     <h2 className='nomeSecao'>Bebidas</h2>
                 </div>
