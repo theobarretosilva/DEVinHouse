975cd9fc930dbd40b5e6bcf321b2f90fdffa05bc
Théo Barreto Silva <barretotheo25@gmail.com>
2022-09-08 11:44:19 -0300

    ex2

---
+++ Modulo1/Exercícios/Semana11/exerciciosSemanais/perfisLTDA/src/themes/themes.jsx
@@ -0,0 +1,17 @@
+export const dark = {
+    cores: {
+        primary: '#35012c',
+        secondary: '#0b525b',
+        backgroudColor: '#00607a',
+        headerColor: '#11001c',
+    },
+};
+
+export const light = {
+    cores: {
+        primary: '#c77dff',
+        secondary: '#b3e9c7',
+        backgroudColor: '#9ba9ff',
+        headerColor: '#9984d4',
+    },
+};
