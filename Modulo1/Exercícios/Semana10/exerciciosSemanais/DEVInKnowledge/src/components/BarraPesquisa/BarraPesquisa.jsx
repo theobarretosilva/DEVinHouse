7bd799aaebc6becb82d0bcfda56b8b65d2a79122
Théo Barreto Silva <barretotheo25@gmail.com>
2022-09-27 17:57:50 -0300

    upload

--- Modulo1/Exercícios/Semana10/exerciciosSemanais/DEVInKnowledge/src/components/BarraPesquisa/BarraPesquisa.jsx
+++
@@ -1,9 +0,0 @@
-import './BarraPesquisa.css'
-
-export const BarraPesquisa = () => {
-    <div className="pesquisaDicas">
-        <input type="text" id="barraPesquisa" placeholder="Pesquise uma dica" />
-        <button id="pesquisar"><img id="imgPesquisa" src="../../src/assets/imgs/pesquisar.png" alt="Botão para pesquisar" /></button>
-        <button id="limparPesquisa"><img id="imgLimpa" src="../../src/assets/imgs/limpar.png" alt="Botão para limpar a pesquisa" /></button>
-    </div>
-}
