491a140cd415012b971ac951751957b8723e01a1
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-04 20:19:09 -0300

    upload

--- Modulo2/Semana2/exerciciosSemanais/back-end/index.js
+++ Modulo2/Semana2/exerciciosSemanais/back-end/index.js
@@ -1,5 +1,76 @@
+const pizzas = [
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+    {
+        "id": "",
+        "name": "",
+        "description": "",
+        "price": "",
+        "ingredients": "",
+
+    },
+];
+
 const express = require('express');
 const app = express();
+
+app.get('/pizzas', (response) => {
+    response.json()
+})
 
 app.listen(3333, () => {
     console.log("Servidor online!");
