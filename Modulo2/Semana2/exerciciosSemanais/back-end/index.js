b41429c9ead23cb5130a65b2d336e4111295c7d4
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-07 22:12:42 -0300

    upload

--- Modulo2/Semana2/exerciciosSemanais/back-end/index.js
+++ Modulo2/Semana2/exerciciosSemanais/back-end/index.js
@@ -1,77 +1,31 @@
-const pizzas = [
-    {
-        "id": "1",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "2",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "3",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "4",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "5",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "6",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "7",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-    {
-        "id": "8",
-        "name": "",
-        "description": "",
-        "price": "",
-        "ingredients": "",
-
-    },
-];
-
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
 
-app.get('/pizzas', (response) => {
-    response.json()
-})
+let pizzas = [];
+
+app.use(express.json());
 
 app.listen(3333, () => {
     console.log("Servidor online!");
 });
+
+app.get('/pizzas', (request, response) => {
+    response.json(pizzas)
+})
+
+app.post('/pizzas', (request, response) => {
+    const {name, description, url, price, ingredients} = request.body;
+    const pizza = {
+        id: uuidv4(),
+        name,
+        description,
+        url,
+        price,
+        ingredients
+    };
+
+    pizzas.push(pizza);
+
+    response.status(201).json(pizza)
+})
