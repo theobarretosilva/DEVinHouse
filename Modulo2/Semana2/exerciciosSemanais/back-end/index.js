f55ec807dc55d23f4e54f7694ac371cdc5ce8b3b
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-10 16:18:54 -0300

    ex 2 e ex 3 prontos

--- Modulo2/Semana2/exerciciosSemanais/back-end/index.js
+++ Modulo2/Semana2/exerciciosSemanais/back-end/index.js
@@ -1,8 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
 const express = require('express');
+const { response } = require('express');
 const app = express();
 
 let pizzas = [];
+let solicitations = [];
 
 app.use(express.json());
 
@@ -11,16 +13,17 @@ app.listen(3333, () => {
 });
 
 app.get('/pizzas', (request, response) => {
-    response.json(pizzas)
+    const nameQuery = request.query.name;
+    const pizzasFiltered = pizzas.filter(pizza => pizza.name.toLowerCase().includes(nameQuery));
+    response.json(pizzasFiltered)
 })
 
 app.post('/pizzas', (request, response) => {
-    const {name, description, url, price, ingredients} = request.body;
+    const {name, description, price, ingredients} = request.body;
     const pizza = {
         id: uuidv4(),
         name,
         description,
-        url,
         price,
         ingredients
     };
@@ -28,4 +31,27 @@ app.post('/pizzas', (request, response) => {
     pizzas.push(pizza);
 
     response.status(201).json(pizza)
+})
+
+app.get('/solicitations', (request, response) => {
+    response.json(solicitations)
+})
+
+app.post('/solicitations', (request, response) => {
+    const {clientName, clientCpf, clientAddress, clientPhone, payment, obs, pizzaOrder} = request.body;
+    const solicitation = {
+        id: uuidv4(),
+        clientName,
+        clientCpf,
+        clientAddress,
+        clientPhone,
+        payment, 
+        obs,
+        pizzaOrder,
+        status: "Em producão"
+    };
+
+    solicitations.push(solicitation);
+
+    response.status(201).json(solicitation)
 })
