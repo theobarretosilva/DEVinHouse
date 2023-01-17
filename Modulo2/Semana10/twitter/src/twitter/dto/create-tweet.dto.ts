60964b4b8e9b57d6d6048b7219c0d16cc21ba85b
Théo Barreto Silva <barretotheo25@gmail.com>
2023-01-17 09:49:18 -0300

    Delete Modulo2/Semana10/twitter directory

--- Modulo2/Semana10/twitter/src/twitter/dto/create-tweet.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, MaxLength, MinLength } from "class-validator";
-
-export class CreateTweetDTO {
-    @IsString()
-    @MinLength(1)
-    @MaxLength(280)
-    readonly content: string;
-}
